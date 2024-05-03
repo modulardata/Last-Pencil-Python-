af3f09f755fa2de2a78a9c36d958c39c3be51038
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-05-03 09:17:10 +0200

    Stage 3/5

--- Last Pencil (Python)/task/game.py
+++ Last Pencil (Python)/task/game.py
@@ -1,7 +1,21 @@
-n_panes = int(input('How many pencils would you like to use:\n'))
-first_player = str(input('Who will be the first (John, Jack):\n'))
+n_pencils = int(input('How many pencils would you like to use:\n'))
+player = str(input('Who will be the first (John, Jack):\n'))
+print('|' * n_pencils)
 
-print(n_panes * '|')
-print(first_player + ' is going first!')
+turn = player
 
+while n_pencils > 0:
+    print(f'{turn}\'s turn: ', end='\n')
+    taken = min(n_pencils, int(input()))
+    n_pencils -= taken
+    print('|' * n_pencils)
+    if n_pencils == 0:
+        # print(f'{turn} wins!')
+        break
+    if turn == 'John':
+        turn = 'Jack'
+    else:
+        turn = 'John'
 
+# if n_pencils == 0:
+#     print(f'{turn} loses!')
